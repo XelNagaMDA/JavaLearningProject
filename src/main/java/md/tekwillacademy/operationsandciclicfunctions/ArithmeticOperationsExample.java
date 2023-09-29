a0d49a4afe5c2518d12167fa71db3b89e8307fa5
XelNagaMDA <vpegate@gmail.com>
2023-09-29 15:53:11 +0300

    Added the implementation of Java Operation, Cyclic Functions and Control Structures

---
+++ src/main/java/md/tekwillacademy/operationsandciclicfunctions/ArithmeticOperationsExample.java
@@ -0,0 +1,23 @@
+package md.tekwillacademy.operationsandciclicfunctions;
+
+public class ArithmeticOperationsExample {
+    public static void main(String[] args) {
+        int a = 43;
+        int b = 3;
+
+        int sum = a + b;
+        System.out.println(sum);
+
+        int sub = a - b;
+        System.out.println(sub);
+
+        int multiply = a * b;
+        System.out.println(multiply);
+
+        int division = a / b;
+        System.out.println(division);
+
+        int modulo = a % b;
+        System.out.println(modulo);
+    }
+}
---
+++ src/main/java/md/tekwillacademy/operationsandciclicfunctions/ComparisonOperationsExample.java
@@ -0,0 +1,27 @@
+package md.tekwillacademy.operationsandciclicfunctions;
+
+public class ComparisonOperationsExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        boolean isEqual = a == b;
+        System.out.println(isEqual);
+
+        boolean isNotEqual = a != b;
+        System.out.println(isNotEqual);
+
+        boolean isGreaterThan = a > b;
+        System.out.println(isGreaterThan);
+
+        boolean isLessThan = a < b;
+        System.out.println(isLessThan);
+
+        boolean isGreaterThanOrEqual = a >= b;
+        System.out.println(isGreaterThanOrEqual);
+
+        boolean isLessThanOrEqual = a <= b;
+        System.out.println(isLessThanOrEqual);
+
+    }
+}
